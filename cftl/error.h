836093a93a95722375d4248e7db05797343eb9a4
vx-clutch <owestness@gmail.com>
2024-11-25 15:12:14 -0500

    simplified error

--- cftl/error.h
+++ cftl/error.h
@@ -1,19 +1,33 @@
 #ifndef ERROR_H
 #define ERROR_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include <stdio.h>
+#include <stdlib.h>
 
-void fatal_error(const char *message);
+// ANSI color codes
+static const char *RESET = "\033[0m";
+static const char *RED = "\033[1;91m";
+static const char *PURPLE = "\033[0;95m";
 
-int error(const char *message);
-
-int warning(const char *message);
-
-int debug(const char *tag, const char *message);
-
-#ifdef __cplusplus
+// Function prototypes
+static inline void fatal_error(const char *message) {
+    (void)fprintf(stderr, "ftl: %sfatal error%s: %s\ncompilation terminated.\n", RED, RESET, message);
+    exit(EXIT_FAILURE);
 }
-#endif
-#endif
+
+static inline int error(const char *message) {
+    (void)fprintf(stderr, "ftl: %serror%s: %s\n", RED, RESET, message);
+    return 0;
+}
+
+static inline int warning(const char *message) {
+    (void)fprintf(stderr, "ftl: %swarning%s: %s\n", PURPLE, RESET, message);
+    return 0;
+}
+
+static inline int debug(const char *tag, const char *message) {
+    (void)printf("debug: %s:\n%s", tag, message);
+    return 0;
+}
+
+#endif // ERROR_H
