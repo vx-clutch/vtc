96cdac09bd76d08cdedf9be03143968a3dd16fd9
vx-clutch <owestness@gmail.com>
2024-11-26 22:06:57 -0500

    save

--- cftl/options/parse_args.c
+++ cftl/options/parse_args.c
@@ -1,6 +1,14 @@
-#include "options.h"
+#include <bits/getopt_ext.h>
+#include <getopt.h>
 
-Options parseArgs(int argc, char **argv) {
-  Options x;
-  return x;
+option[] parseArgs(int argc, char **argv) {
+  int c;
+  int opti = 0;
+  struct option long_options[] = {
+    {"verbose", no_argument, 0, 'v'},
+    {"output", required_argument, 0, 'o'},
+    {0, 0, 0, 0}
+  };
+
+  return long_options;
 }
