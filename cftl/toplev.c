1595b322d99dc2bf179b218949c21ecfb1bf6b65
vx-clutch <owestness@gmail.com>
2024-12-08 15:38:50 -0500

    save

--- cftl/toplev.c
+++ cftl/toplev.c
@@ -1,10 +1,16 @@
-#include "config.h"
+#include "processor.h"
 #include "parse_args.h"
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int toplev(int argc, char **argv) {
   Options options = parse_args(argc, argv);
-  char source[MAXSOURCE];
-
+  char *source;
+  source = processor(source);
+  if (options.E) {
+    (void)printf("%s\n", source);
+    exit(0);
+  }
   return 0;
 }
