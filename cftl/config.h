31f90dcd40df260ff4206d743c2862c073c3da6b
vx_clutch <98831688+vx-clutch@users.noreply.github.com>
2024-12-09 10:19:41 -0500

    Update config.h

--- cftl/config.h
+++ cftl/config.h
@@ -1,12 +1,17 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+
+/* versioning information. */
 #define COMPILER_VERSION "alpha"
 #define COMPILER_YEAR "2024"
 
+/* compile target platform i.e. Ubuntu Jammy. */
 #define COMPILER_META_TARGET_BUFFER 256
 
-#define MAXINPUTBUFFER 1024
+/* max charecters an input buffer can be. */
+#define MAXINPUTBUFFER 4069
+/* max length the name of the output file can be. */
 #define MAXOUTPUTPATH 4069
 
 #endif
