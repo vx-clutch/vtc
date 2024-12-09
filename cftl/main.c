baff9acb964cbce34eb98302d895124ddf0359c5
vx_clutch <98831688+vx-clutch@users.noreply.github.com>
2024-12-09 10:23:00 -0500

    Update main.c

--- cftl/main.c
+++ cftl/main.c
@@ -3,13 +3,10 @@
 #include <assert.h>
 
 #ifndef __linux__
-#error "the ftl software at its current staage of development only features unix support. If you do not like this feel free to make a pull request."
+#error "the ftl software at its current stage of development only features unix support. If you do not like this feel free to make a pull request."
 #endif
 
 int main(int argc, char **argv) {
-  if (argc < 2) {
-    fatal_error("no input files.");
-  }
   (void)toplev(argc, argv);
   return 0;
 }
