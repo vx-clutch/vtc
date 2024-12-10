8576be3886f2891acc6edcd8b028a6914cbff485
vx-clutch <owestness@gmail.com>
2024-12-10 15:49:00 +0000

    save

--- ftl/error.h
+++ ftl/error.h
@@ -9,18 +9,19 @@
 static const char *RESET = "\033[0m";
 static const char *RED = "\033[1;91m";
 static const char *PURPLE = "\033[0;95m";
+static const char *WHITE = "\033[0;91m";
 
 
 /* fatal */
 static inline void fatal_error(const char *message) {
-    (void)fprintf(stderr, "ftl: %sfatal error%s: %s\ncompilation terminated.\n", RED, RESET, message);
+    (void)fprintf(stderr, "%sftl%s: %sfatal error%s: %s\ncompilation terminated.\n", WHITE, RESET, RED, RESET, message);
     exit(EXIT_FAILURE);
 }
 
 static inline void fatal_errorf(const char *format, ...) {
     va_list args;
     va_start(args, format);
-    (void)fprintf(stderr, "ftl: %sfatal error%s: ", RED, RESET);
+    (void)fprintf(stderr, "%sftl%s: %sfatal error%s: ", WHITE, RESET, RED, RESET);
     (void)vfprintf(stderr, format, args);
     (void)fprintf(stderr, "\ncompilation terminated.\n");
     va_end(args);
@@ -29,28 +30,28 @@ static inline void fatal_errorf(const char *format, ...) {
 
 /* error */
 static inline int error(const char *message) {
-    (void)fprintf(stderr, "ftl: %serror%s: %s\n", RED, RESET, message);
+    (void)fprintf(stderr, "%sftl%s: %serror%s: %s\n", WHITE, RESET, RED, RESET, message);
     return 0;
 }
 
 static inline void errorf(const char *format, ...) {
     va_list args;
     va_start(args, format);
-    (void)fprintf(stderr, "ftl: %serror%s: ", RED, RESET);
+    (void)fprintf(stderr, "%sftl%s: %serror%s: ", WHITE, RESET, RED, RESET);
     (void)vfprintf(stderr, format, args);
     va_end(args);
 }
 
 /* warning */
 static inline int warning(const char *message) {
-    (void)fprintf(stderr, "ftl: %swarning%s: %s\n", PURPLE, RESET, message);
+    (void)fprintf(stderr, "%sftl%s: %swarning%s: %s\n", WHITE, RESET, PURPLE, RESET, message);
     return 0;
 }
 
 static inline void warningf(const char *format, ...) {
     va_list args;
     va_start(args, format);
-    (void)fprintf(stderr, "ftl: %swarning%s: ", PURPLE, RESET);
+    (void)fprintf(stderr, "%sftl%s: %swarning%s: ", WHITE, RESET, PURPLE, RESET);
     (void)vfprintf(stderr, format, args);
     va_end(args);
 }
