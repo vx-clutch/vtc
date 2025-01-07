c028e23047a012607dba858d4d7cc2fc7bbfc6b8
vx-clutch <owestness@gmail.com>
2025-01-07 18:22:00 -0500

    save

--- vtc/testsuite/vtc.src/libgo/test_libgo.c
+++ vtc/testsuite/vtc.src/libgo/test_libgo.c
@@ -6,7 +6,7 @@ int
 main(void)
 {
   char *header = get_header_for("linuxx64");
-  char *populated = populate(header, COMPILER_NAME, "foo.ftl");
+  char *populated = populate(header, COMPILER_NAME, "foo.vl");
   printf("Base Header:\n%s\n\n", header);
   printf("Populated Header:\n%s\n", populated);
   return 0;
